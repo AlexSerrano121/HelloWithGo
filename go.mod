e07557fb227ad455311e8ae1eece651e54734586
AlexSerrano121 <alexander10139@gmail.com>
2023-12-06 20:39:13 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AlexSerrano121/HelloWithGo
+
+go 1.21.4
---
+++ utils.go
@@ -0,0 +1,13 @@
+package utils
+
+import "fmt"
+
+func HelloWorld() {
+	fmt.Println("Hello wolrd with Go!!!")
+}
+
+/*
+Crear nuestro propio módulo
+1. Creamos nuestro repositorio en github o donde deseemos.
+2. Crear nuestro propio paquete con su respectiva función, el nombre de la función debe estar en mayuscula para que Go entienda que es pública.
+*/
